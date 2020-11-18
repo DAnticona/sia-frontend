2a7ec035ebfc75cf63da1ab7777f49999c452519
David Anticona <danticona@wollcorp.com>
2020-11-18 17:37:21 -0500

    salida de wc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,23 @@ import { AppRoutingModule } from './app-routing.module';
 // Modulos
 import { PagesModule } from './pages/pages.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 // Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
 import { DialogAgenciasComponent } from './components/dialogs/dialog-agencias/dialog-agencias.component';
 
 @NgModule({
-	declarations: [AppComponent, LoginComponent, DialogAgenciasComponent],
-	imports: [BrowserModule, FormsModule, PagesModule, HttpClientModule, AppRoutingModule],
-	providers: [],
-	bootstrap: [AppComponent],
+  declarations: [AppComponent, LoginComponent, DialogAgenciasComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    PagesModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
